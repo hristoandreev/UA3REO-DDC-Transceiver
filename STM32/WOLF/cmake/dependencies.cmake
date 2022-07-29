00e3d39a872eed2d97c191100aa4d1dd1b289afd
Hristo Andreev <handreev@nimloth.eu>
2022-07-29 10:37:37 +0300

    add: cmake build files

---
+++ STM32/WOLF/cmake/dependencies.cmake
@@ -0,0 +1,19 @@
+cmake_minimum_required(VERSION 3.14)
+
+include(FetchContent)
+
+######################################## CMSIS-5 ###################################################
+FetchContent_Declare(
+    cmsis_5
+    URL https://github.com/ARM-software/CMSIS_5/archive/refs/heads/master.zip
+)
+FetchContent_MakeAvailable(cmsis_5)
+###################################### END CMSIS-5 #################################################
+
+################################## lcd-image-converter #############################################
+FetchContent_Declare(
+    lcd-image-converter
+    URL https://sourceforge.net/projects/lcd-image-converter/files/latest/download?use_mirror=autoselect
+)
+FetchContent_MakeAvailable(lcd-image-converter)
+############################## END lcd-image-converter #############################################
